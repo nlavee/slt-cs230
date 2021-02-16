f8b6d88d8ad538cfb8a445d44d1ddc32f333f6dd
Ubuntu <ubuntu@ip-172-31-14-82.us-east-2.compute.internal>
2021-02-16 10:40:55 +0000

    adding tf stuff

---
+++ transformer-slt-tf/custom_model.py
@@ -0,0 +1,19 @@
+import opennmt
+
+class MyCustomTransformer(opennmt.models.Transformer):
+    def __init__(self):
+        super().__init__(
+            source_inputter=opennmt.inputters.WordEmbedder(embedding_size=512),
+            target_inputter=opennmt.inputters.WordEmbedder(embedding_size=512),
+            num_layers=2,
+            num_units=512,
+            num_heads=8,
+            ffn_inner_dim=2048,
+            dropout=0.1,
+            attention_dropout=0.1,
+            ffn_dropout=0.1,
+        )
+
+    # Here you can override any method from the Model class for a customized behavior.
+
+model = MyCustomTransformer
