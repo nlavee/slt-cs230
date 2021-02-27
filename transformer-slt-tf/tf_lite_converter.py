10d111e01922202ca411759bd9fc2c2057837542
Vu Nguyen <anhvu.nguyenlam@gmail.com>
2021-02-26 18:19:52 -0800

    Add a simple script to turn TF model to TF-Lite.

---
+++ transformer-slt-tf/tf_lite_converter.py
@@ -0,0 +1,31 @@
+import tensorflow as tf
+
+def main(argv):
+    """
+    Simple script to attempt turning a full TF model into a TF-lite model.
+    """
+    try:
+        opts, args = getopt.getopt(argv,
+                                   "m",
+                                   ["model_path="])
+    except getopt.GetoptError:
+        logging.error('wrong command line param')
+        sys.exit(2)
+
+    model_path = ''
+    for opt, arg in opts:
+        if opt in ("-m", "--model_path"):
+            model_path= arg
+        else:
+            logging.error('Wrong param, only -m or --model_path')
+            sys.exit(2)
+
+    logging.info(f"Model Path: {model_path}")
+    converter = tf.lite.TFLiteConverter.from_saved_model(model_path)
+    tflite_model = converter.convert()
+    open("converted_model.tflite", "wb").write(tflite_model)
+
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
