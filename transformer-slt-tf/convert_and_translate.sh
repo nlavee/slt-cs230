437e556086dccd032841c58977f587291cf83bcd
Vu Nguyen <anhvu.nguyenlam@gmail.com>
2021-03-08 23:36:09 -0800

    Add option to specify yml file as well.

--- transformer-slt-tf/convert_and_translate.sh
+++ transformer-slt-tf/convert_and_translate.sh
@@ -21,13 +21,29 @@ else
   exit 2
 fi
 
+echo "Do you want to run model with run_aslg.yml? (y/n)"
+read run_default_config
+CONFIG="run_aslg.yml"
+if [[ run_default_config == "y" ]];
+then
+  echo "Using run_aslg.yml"
+elif [[ run_default_config == "n" ]];
+then
+  echo "Please input a yml file:"
+  read yml_file
+  echo "Using supplied yml file: ${yml_file}"
+  CONFIG="$yml_file"
+else
+  echo "Invalid choice. Exiting"
+  exit 2
+
 # Construct date path.
 DATE=$(date +"%Y_%m_%d_%H_%M")
 EXPORT_DIR="tf_lite_export_${DATE}"
 echo "Outputting the lite model to dir: $EXPORT_DIR"
 
 # Convert the model into a lite version
-onmt-main --config run_aslg.yml export --export_dir "$EXPORT_DIR" --export_format "$LITE_MODEL_PARAM"
+onmt-main --config "$CONFIG" export --export_dir "$EXPORT_DIR" --export_format "$LITE_MODEL_PARAM"
 
 echo "===================================================="
 echo "Your lite model has been exported to dir: $EXPORT_DIR"
